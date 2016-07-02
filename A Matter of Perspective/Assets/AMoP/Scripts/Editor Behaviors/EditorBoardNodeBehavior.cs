8dee230d377d5e51ad4b70f27e26476b4f56e207
chadrc <chadrcollins91@gmail.com>
2016-07-02 12:38:29 -0400

    Color codeing to editor nodes

--- A Matter of Perspective/Assets/AMoP/Scripts/Editor Behaviors/EditorBoardNodeBehavior.cs
+++ A Matter of Perspective/Assets/AMoP/Scripts/Editor Behaviors/EditorBoardNodeBehavior.cs
@@ -7,7 +7,35 @@ public class EditorBoardNodeBehavior : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawSphere(transform.position, .75f);
+		var clr = Color.white;
+
+		switch (data.Type) {
+		case BoardNodeType.Basic:
+			clr = Color.green;
+			break;
+
+		case BoardNodeType.Pool:
+			clr = Color.blue;
+			break;
+
+		case BoardNodeType.Null:
+			clr = Color.black;
+			break;
+
+		case BoardNodeType.Moving:
+			clr = Color.red;
+			break;
+
+		case BoardNodeType.Block:
+			clr = Color.grey;
+			break;
+
+		case BoardNodeType.Vortex:
+			clr = Color.cyan;
+			break;
+		}
+
+        Gizmos.color = clr;
+        Gizmos.DrawSphere(transform.position, .5f);
     }
 }
