a820b69dafe735ea04e74310cd3ea3376e0f6d94
chadrc <chadrcollins91@gmail.com>
2016-07-16 21:34:07 -0400

    Added board size to board data

--- A Matter of Perspective/Assets/AMoP/Scripts/Board/BoardData.cs
+++ A Matter of Perspective/Assets/AMoP/Scripts/Board/BoardData.cs
@@ -12,18 +12,25 @@ public class BoardData : ScriptableObject
     public string Description { get { return description; } set { description = value; } }
 
     [SerializeField]
+    [Range(3, 6)]
+    private int boardSize = 6;
+    public int BoardSize { get { return boardSize; } set { if (value >= 3 && value <= 6) { boardSize = value; } } }
+
+    [SerializeField]
     private List<BoardNodeData> nodes = new List<BoardNodeData>();
     public List<BoardNodeData> Nodes { get { return new List<BoardNodeData>(nodes); } }
 
+    public int MaxNodes { get { return boardSize * boardSize * boardSize; } }
+
     public void AddNode()
     {
-        if (nodes.Count < 6 * 6 * 6)
+        if (nodes.Count < MaxNodes)
         {
             nodes.Add(new BoardNodeData());
         }
         else
         {
-            throw new System.InvalidOperationException("Cannot have more than " + 6 * 6 * 6 + " nodes in a board.");
+            throw new System.InvalidOperationException("Cannot have more than " + MaxNodes + " nodes in this board.");
         }
     }
 
