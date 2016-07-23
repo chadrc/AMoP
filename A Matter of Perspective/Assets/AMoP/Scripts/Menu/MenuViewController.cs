5c8a04139ecbf955a4c715c64aba5c2bf1fccff0
chadrc <chadrcollins91@gmail.com>
2016-07-23 15:16:02 -0400

    Added options menu Addtional buttons for options and store on main menu Added main audio mixer

--- A Matter of Perspective/Assets/AMoP/Scripts/Menu/MenuViewController.cs
+++ A Matter of Perspective/Assets/AMoP/Scripts/Menu/MenuViewController.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 [RequireComponent (typeof(CanvasGroup))]
 public class MenuViewController : MonoBehaviour
 {
+    [SerializeField]
+    private CanvasGroup optionsCanvasGroup;
+
+    [SerializeField]
+    private VerticalLayoutGroup buttonLayoutGroup;
+
     private CanvasGroup canvasGroup;
 
     public void Show()
@@ -14,8 +21,8 @@ public class MenuViewController : MonoBehaviour
     private void Awake()
     {
         canvasGroup = GetComponent<CanvasGroup>();
-
         LevelBehavior.GameEnd += OnGameEnd;
+        ScreenChangeListeningBehavior.ScreenChanged += onScreenChanged;
     }
 
     private void OnDestroy()
@@ -29,8 +36,33 @@ public class MenuViewController : MonoBehaviour
         canvasGroup.Hide();
     }
 
+    public void OptionsButtonPressed()
+    {
+        canvasGroup.Hide();
+        optionsCanvasGroup.Show();
+    }
+
+    public void StoreButtonPressed()
+    {
+
+    }
+
     private void OnGameEnd()
     {
 
+    }
+
+    private void onScreenChanged(int width, int height)
+    {
+        if (width > height)
+        {
+            buttonLayoutGroup.padding = new RectOffset(400, 400, 100, 100);
+            buttonLayoutGroup.spacing = 50;
+        }
+        else
+        {
+            buttonLayoutGroup.padding = new RectOffset(150, 150, 200, 200);
+            buttonLayoutGroup.spacing = 100;
+        }
     }
 }
---
+++ A Matter of Perspective/Assets/AMoP/Scripts/Menu/OptionsViewController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(CanvasGroup))]
+public class OptionsViewController : MonoBehaviour
+{
+    [SerializeField]
+    private CanvasGroup mainMenuCanvas;
+
+    [SerializeField]
+    private GridLayoutGroup signInButtonGrid;
+
+    [SerializeField]
+    private GridLayoutGroup volumeSliderGrid;
+
+    private CanvasGroup canvasGroup;
+
+    void Awake()
+    {
+        ScreenChangeListeningBehavior.ScreenChanged += onScreenChanged;
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.Hide();
+    }
+
+    public void OnMaterVolumeSliderChanged(float value)
+    {
+
+    }
+
+    public void OnMusicVolumeSliderChanged(float value)
+    {
+
+    }
+
+    public void OnSoundVolumeSliderChanged(float value)
+    {
+
+    }
+
+    public void OnCloseButtonPressed()
+    {
+        canvasGroup.Hide();
+        mainMenuCanvas.Show();
+    }
+    
+    private void onScreenChanged(int width, int height)
+    {
+        if (width > height)
+        {
+            signInButtonGrid.constraintCount = 1;
+            volumeSliderGrid.constraint = GridLayoutGroup.Constraint.FixedRowCount;
+        }
+        else
+        {
+            signInButtonGrid.constraintCount = 2;
+            volumeSliderGrid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        }
+    }
+}
