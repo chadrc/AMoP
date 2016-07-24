3395ccdeac59e2f0783ad7e328a33f47a86b8493
chadrc <chadrcollins91@gmail.com>
2016-07-24 12:27:44 -0400

    Text change to instructions in LevelOneInfo

--- A Matter of Perspective/Assets/AMoP/Scripts/BoardInfoClasses/LevelOneInfo.cs
+++ A Matter of Perspective/Assets/AMoP/Scripts/BoardInfoClasses/LevelOneInfo.cs
@@ -6,6 +6,6 @@ public class LevelOneInfo : BaseBoardInfo
     protected override void setup()
     {
         infoController.Highlight(0, 0);
-        infoController.SetText("This is a node, it stores energy. Tap and drag from a full node to an empty one to transfer the energy.");
+        infoController.SetText("This is a node, it stores energy. Touch and drag from a full node to an empty one to transfer the energy.");
     }
 }
